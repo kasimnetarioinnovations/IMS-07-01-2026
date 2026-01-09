c350ad74c7369283fab021d261db33343ba28d94
Akash <akash.kumar@netarioinnovations.com>
2026-01-09 16:48:50 +0530

    create download pdf

--- clients/src/components/LayoutsCopy/MainLayouts.jsx
+++ clients/src/components/LayoutsCopy/MainLayouts.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, matchPath } from "react-router-dom";
+
+
 
 const MainLayouts = () => {
   const location = useLocation();
@@ -11,10 +13,19 @@ const MainLayouts = () => {
     location.pathname === "/add-product" ||
     location.pathname === "/product/edit" ||
     location.pathname === "/viewproduct" ||
-    location.pathname === "/create-purchase-orders" ||
     location.pathname === "/m/create-creditnote" ||
     location.pathname === "/skeleton" || 
-    location.pathname === "/createinvoice" || 
+    // for edit view product
+    matchPath("/product/edit/:id", location.pathname) || 
+    matchPath("/product/view/:id", location.pathname) ||
+    // for invoice, quotation, credit note
+      matchPath("/createinvoice/:customerId", location.pathname) ||
+       matchPath("/create-quotition/:customerId", location.pathname) ||         
+       location.pathname === "/credit-note" ||
+       // for purchaseorder, debit note
+       matchPath("/create-purchase-orders/:supplierId", location.pathname) ||
+       matchPath("/create-supplier-debitnote/:supplierId", location.pathname)
+
     location.pathname === "/create-debitnote" ;
   return (
     <div
